4235c0cd0c658de4f183cf7b379b6bdac0c386ff
Kevin <long@Andys-MacBook-Pro.local>
2015-11-17 02:29:15 -0500

    Updated with a function for computing hypothesis costs, costFunction() and a feature scaling function, meanNormalization(). Mean Normalization Does not full work yet.

--- LinearRegression.c
+++ LinearRegression.c
@@ -1,21 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
+#include <math.h>
 #include "csvparse.c"
+
+double costFunction();
+void meanNormalization();
 
 int main(int argc, char **argv){
 	char* filename = argv[1];
+	/*This is the number of features provided on the command line*/
+	/*The csv should contain values for each x value and the result, per example*/
 	int features = atoi(argv[2]);
-	int examples = atoi(argv[3]);
-	int **X = malloc(examples * sizeof(int*));
+	int examples = atoi(argv[3]); /*Number of training examples provided*/
+	double cost = DBL_MAX; /*Cost for the current hypothesis, set arbitratily high*/
+	/*Values for the coefficients in the hypothesis function*/
+	double *theta = malloc(features * sizeof(double));
+	double **X = malloc(examples * sizeof(double*));
 	for(int i = 0; i < examples; i++){
-		X[i] = malloc(features * sizeof(int));
+		X[i] = malloc(features * sizeof(double));
 	}
-	X = parse(features, examples, X, filename);
-	for(int i = 0; i < examples; i++){
-		for(int j = 0; j< features; j++){
-			printf("%d ", X[i][j]);
+	double *Y = malloc(examples * sizeof(double));
+	parse(features, examples, X, Y, filename);
+	for(int i = 0; i < features; i++){
+		theta[i] = 0;
+	}
+	double **meanAndRange = malloc((features - 1) * sizeof(double*));
+	for(int i = 0; i < features - 1; i++){
+		meanAndRange[i] = malloc(2 * sizeof(double));
+	}
+	/*meanNormalization();         GOTTA MAKE THIS WORK!!!!!!*/
+	/*****************************************************************************/
+	/*This part will be inside the gradient descent function*/
+	cost = costFunction(theta, X, Y, features, examples);
+	printf("%f\n", cost);
+	/*****************************************************************************/
+}
+
+double costFunction(int *theta, double **X, double *Y, int features, int examples){
+	double cost;
+	double runningSum;
+	for(int i = 0; i< examples; i++){
+		double xValue = 0;
+		for(int j = 0; j < features; j++){
+			xValue += X[i][j] * theta[j];
 		}
-		printf("\n");
+		runningSum += pow(xValue - Y[i], 2);
+	}
+	cost = (.5 * examples) * runningSum;
+	return cost;
+}
+
+void meanNormalization(double **X, double **Y, double **meanAndRange, int features, int examples){
+	double min;
+	double max;
+	double mean;
+	for(int i = 1; i < features; i++){
+		min = X[0][i];
+		max = X[0][i];
+		mean = 0;
+		for(int j = 0; j < examples; j++){
+			if(X[j][i] > max){
+				max = X[j][i];
+			}
+			if(X[i][j] < min){
+				min = X[j][i];
+			}
+			mean += X[j][i];
+		}
+		mean /= features - 1;
+		meanAndRange[i -1][0] = mean;
+		meanAndRange[i - 1][1] = max - min;
+	}
+	for(int i = 0; i < examples; i++){
+		for(int j = 1; j < features; j++){
+			X[i][j] = (X[i][j] - meanAndRange[j - 1][0]) / meanAndRange[j - 1][1];
+		}
 	}
 }
